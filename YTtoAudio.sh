5369aaf546cfdc188e4f9019ac99ce2fae523228
bashnett <nishanbasnet3636@gmail.com>
2023-06-14 07:46:08 +0545

    Update

--- YTtoAudio.sh
+++ YTtoAudio.sh
@@ -6,18 +6,17 @@ sudo apt install ffmpeg
 sudo apt install yt-dlp
 
 ###
-echo "USAGE-- enter url then filename you want to save it as 
-      Example:www.youtube.com/3666879 asd"
+read -p "Enter filename you want to save it as:   " f 
 
 read -p "Enter the URL:   " u
 
 
-yt-dlp $u -o $1
+yt-dlp $u -o $f
 
 read -p "DO you want to Convert it to Audio or not (yes or no)" q
 case $q in
 	y|Y|yes|YES)
-		ffmpeg -i $1.webm -vn $1.mp3
+		ffmpeg -i $f.webm -vn $f.mp3
 		rm *.webm
 		echo "Thank you for using this Script"
 		;;
